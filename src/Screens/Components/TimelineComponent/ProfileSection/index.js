5800c79114f69cfe9309a6af74983b55e2933bf4
ruchi1234 <info.webnexus@gmail.com>
2020-11-18 18:40:01 +0530

    Ashish sir work, login language issue

--- src/Screens/Components/TimelineComponent/ProfileSection/index.js
+++ src/Screens/Components/TimelineComponent/ProfileSection/index.js
@@ -76,7 +76,7 @@ class PointPain extends Component {
         }).then((responce) => {
             this.props.getData();
             this.setState({ loaderImage: false });
-            axios.put('https://api-eu.cometchats.io/v2.0/users/' + this.state.personalinfo.info.profile_id.toLowerCase(), {
+            axios.put('https://api-eu.cometchat.io/v2.0/users/' + this.state.personalinfo.info.profile_id.toLowerCase(), {
                 avatar: this.state.uploadedimage
             },{
                 headers: {
