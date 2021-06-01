92197ed2c05dcb6b5a246cc002231b83aeed094d
Ruchi-agrawal <ruchi.agarwal2005@gmail.com>
2021-06-01 11:57:10 +0530

    some few change

--- src/Screens/Components/CardInput/getPriceId.js
+++ src/Screens/Components/CardInput/getPriceId.js
@@ -7,11 +7,9 @@ export function getPriceId(type) {
   }
   if (env === "DEV") {
     let price = type == "Doc Around The Clock" ? "price_1IiFBiH4UyTD79BwHpJf72tD" : "price_1IiFEJH4UyTD79BwEEdzAZe1"
-    console.log('price1', price)
     return price;
   } else {
     let price = type == "Doc Around The Clock" ? "price_1IiFBnH4UyTD79Bw5bl9Iu6o" : "price_1IiFEsH4UyTD79BwpNveERog"
-    console.log('price2', price)
     return price;
   }
 }
