15d114ac1f3ece48210cec63893ea791bc0a0e7f
Ruchi-agrawal <ruchi.agarwal2005@gmail.com>
2021-12-23 14:36:27 +0530

    error resolve and speciality jira issue

--- src/Screens/VirtualHospital/SpaceManagement/index.js
+++ src/Screens/VirtualHospital/SpaceManagement/index.js
@@ -392,7 +392,7 @@ class Index extends Component {
                   </a>
                 </Grid>
                 <Grid>
-                  {this.state.speciality._id ? <label>{edit} <span className="spacemanageDel" onClick={() => { this.handleOpenWarn(this.state.speciality._id) }}><img src={require('assets/virtual_images/bin.svg')} alt="" title="" /> {DeleteSpeciality}</span></label> :
+                  {this.state.speciality._id ? <label>{edit} <span className="spacemanageDel" onClick={() => handleOpenWarn(this.state.speciality._id) }><span className="deleteimg"></span> {DeleteSpeciality}</span></label> :
                     <label>{AddSpeciality}</label>}
                 </Grid>
               </Grid>
@@ -532,7 +532,7 @@ class Index extends Component {
                       </Grid>
                     </Grid>
                     <Grid className="spclSaveBtn saveNclose">
-                      <Button onClick={this.SaveSpeciality}>
+                      <Button onClick={()=>SaveSpeciality(this)}>
                         {save_and_close}
                       </Button>
                     </Grid>
