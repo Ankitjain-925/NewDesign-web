7c4e77b771e25450668984805dc6ef553fd710d7
ruchi1234 <info.webnexus@gmail.com>
2021-12-28 14:16:17 +0530

    working on patientDetails and virtual_hospital.css

--- src/Screens/VirtualHospital/PatientDetails/index.js
+++ src/Screens/VirtualHospital/PatientDetails/index.js
@@ -287,7 +287,7 @@ class Index extends Component {
                                         </Tabs>
                                     </AppBar>
                                 </Grid>
-                                <Grid container direction="row">
+                                <Grid container direction="row" className="mainMenuAllSec">
                                     {/* <VHfield name="ANkit" Onclick2={(name, value)=>{this.myclick(name , value)}}/> */}
 
 
